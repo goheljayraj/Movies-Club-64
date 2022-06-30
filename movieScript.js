4310e96140abbc08a47f955e3bf723307d8cbfbe
goheljayraj <goheljayraj@gmail.com>
2022-06-30 19:40:07 +0530

    fixed categoy and rating for api

--- movieScript.js
+++ movieScript.js
@@ -19,7 +19,7 @@ function showMovies(data){
     main.innerHTML = ' ';
     data.forEach(movie =>{
 
-        const{title, poster_path, vote_average, release_date} = movie;
+        const{title, poster_path, vote_average, release_date, genre_ids} = movie;
         console.log(title);
         const movieEl = document.createElement("div");
         movieEl.classList.add('swiper-slide')
@@ -39,12 +39,14 @@ function showMovies(data){
 
                 <div class="category-rating">
                     <div class="category">
-                    <a href="">Horror</a>, <a href="">Mistery</a>, <a href="">Thriller</a>
+                    <a href="">${getGenre(genre_ids[0])}</a>
+                    <a href="">${getGenre(genre_ids[1])}</a> 
+                    <a href="">${getGenre(genre_ids[2])}</a>
                     </div>
-
+                        
                     <div class="rating">
                                     <img src="res/star-img.png" alt=""/>
-                                    <span class="val">${vote_average}</span>
+                                    <span class="val" style="color: ${getColor(vote_average)}">${vote_average.toFixed(1)}</span>
                     </div>
                 </div>
             </div>
@@ -54,4 +56,32 @@ function showMovies(data){
         `;
         main.appendChild(movieEl);
     })
+}
+
+
+function getColor(vote){
+    if(vote>=8) return 'lime';
+    else if(vote>=5) return 'orange';
+    else return 'red';
+}
+
+// {"genres":[{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]}
+
+function findId(genreData, idToLookFor) {
+
+    for (let i = 0; i < genreData.length; i++) {
+        if (genreData[i].id == idToLookFor) {
+            return(genreData[i].name);
+        }
+    }
+}
+
+function getGenre(id){
+    let genreData = [{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}];
+
+    var genreName =  findId(genreData, id);
+    if(genreName==undefined){
+        return "";
+    }
+    else return genreName;
 }
--- script.js
+++ script.js
@@ -19,8 +19,8 @@ function showMovies(data){
     main.innerHTML = ' ';
     data.forEach(movie =>{
 
-        const{title, poster_path, vote_average, release_date} = movie;
-        console.log(title);
+        const{title, poster_path, vote_average, release_date, genre_ids} = movie;
+        console.log(genre_ids)
         const movieEl = document.createElement("div");
         movieEl.classList.add('swiper-slide')
         movieEl.innerHTML = `
@@ -34,17 +34,22 @@ function showMovies(data){
             <div class="bottom-text">
                 <div class="movie-name">
 <!--                    <span>2021</span>-->
-                    <strong>${release_date} : ${title}</strong>
+                    <strong>${release_date.substring(0,4)} : ${title}</strong>
                 </div>
 
                 <div class="category-rating">
                     <div class="category">
-                    <a href="">Horror</a>, <a href="">Mistery</a>, <a href="">Thriller</a>
+                     <a href="">${getGenre(genre_ids[0])}</a>
+                    
+                     <a href="">${getGenre(genre_ids[1])}</a>
+                     <a href="">${getGenre(genre_ids[2])}</a>
+                     <a href="">${getGenre(genre_ids[3])}</a>
+                     
                     </div>
 
                     <div class="rating">
                                     <img src="res/star-img.png" alt=""/>
-                                    <span class="val">${vote_average}</span>
+                                    <span class="val" style="color: ${getColor(vote_average)}">${vote_average.toFixed(1)}</span>
                     </div>
                 </div>
             </div>
@@ -82,7 +87,7 @@ function getLatest(lrl){
 function topratedShow(topdata){
     post.innerHTML= ' ';
     topdata.forEach(topmovie => {
-        const {title, poster_path, vote_average, release_date} = topmovie;
+        const {title, poster_path, vote_average, release_date, genre_ids} = topmovie;
 
         const movieEl = document.createElement("div");
         movieEl.classList.add('post-box')
@@ -97,17 +102,19 @@ function topratedShow(topdata){
             <div class="bottom-text">
                 <div class="movie-name">
 <!--                    <span>2021</span>-->
-                    <strong>${release_date} : ${title}</strong>
+                    <strong>${release_date.substring(0,4)} : ${title}</strong>
                 </div>
 
                 <div class="category-rating">
                     <div class="category">
-                    <a href="">Horror</a>, <a href="">Mistery</a>, <a href="">Thriller</a>
+                    <a href="">${getGenre(genre_ids[0])}</a>
+                     <a href="">${getGenre(genre_ids[1])}</a>
+                      <a href="">${getGenre(genre_ids[2])}</a>
                     </div>
 
                     <div class="rating">
                                     <img src="res/star-img.png" alt=""/>
-                                    <span class="val">${vote_average}</span>
+                                    <span class="val" style="color: ${getColor(vote_average)}">${vote_average.toFixed(1)}</span>
                     </div>
                 </div>
             </div>
@@ -119,3 +126,33 @@ function topratedShow(topdata){
 
     });
 }
+
+function getColor(vote){
+    if(vote>=8) return 'lime';
+    else if(vote>=5) return 'orange';
+    else return 'red';
+}
+
+// {"genres":[{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]}
+
+function findId(genreData, idToLookFor) {
+
+    for (let i = 0; i < genreData.length; i++) {
+        if (genreData[i].id == idToLookFor) {
+            return(genreData[i].name);
+        }
+    }
+}
+
+function getGenre(id){
+    let genreData = [{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}];
+
+    var genreName =  findId(genreData, id);
+    if(genreName==undefined){
+        return "";
+    }
+    else return genreName;
+
+}
+// url querry parrameters...
+
